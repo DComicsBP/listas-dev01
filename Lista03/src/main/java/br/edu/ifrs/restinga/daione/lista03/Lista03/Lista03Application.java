abc029a910e736ab94248cf5b08b9a41332b8bcf
DAIONE CAROLINE BALDUZZI PAVAN <DAIONE CAROLINE BALDUZZI PAVAN>
2018-09-28 08:44:26 -0300

    adicionando a lista 3

---
+++ Lista03/src/main/java/br/edu/ifrs/restinga/daione/lista03/Lista03/Lista03Application.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.restinga.daione.lista03.Lista03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista03Application.class, args);
+	}
+}
---
+++ Lista03/src/test/java/br/edu/ifrs/restinga/daione/lista03/Lista03/Lista03ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ifrs.restinga.daione.lista03.Lista03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lista03ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
