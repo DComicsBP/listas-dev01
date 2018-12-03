b1cf4fc362e3e5122400b111c4ebcd64a3b743d9
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-12-02 22:25:11 -0200

    exercicio 4 finalizado

--- Lista04/lista04/src/App.js
+++ Lista04/lista04/src/App.js
@@ -3,12 +3,13 @@ import '../../../Lista04/lista04/node_modules/bootstrap/dist/css/bootstrap.min.c
 import Exercicio01 from './exercicio01/Exercicio01';
 import Exercicio02 from './exercicio02/Exercicio02';
 import Exercicio03 from './exercicio03/Exercicio03';
+import Exercicio04 from './exercicio04/Exercicio04';
 class App extends Component {
   render() {
     return (
       <div className="App container">
         <h1>Eu funciono</h1>
-        <Exercicio03/>
+        <Exercicio04/>
       </div>
     );
   }
--- Lista04/lista04/src/exercicio04/Exercicio04.js
+++ Lista04/lista04/src/exercicio04/Exercicio04.js
@@ -2,12 +2,40 @@ import React, {Component} from 'react';
 export default class Exercicio04 extends Component{
     constructor(){
         super(); 
+        this.state = { consoantes:0, vogais:0, caracteres:0, outros: 0, especial:0 }
     }
+
+    verificaDigito(e){
+        this.setState({
+            vogais: (e.target.value.match(/[aeiou]/g) || []).length, 
+            consoantes: (e.target.value.match(/[b-df-hj-np-tv-z]/g) || []).length,
+            especial: (e.target.value.match(/[^0-9A-Za-z]/g) || []).length,
+            outros:(e.target.value.match(/\d/g) || []).length,
+            caracteres: (e.target.value.match(/\d/g) || []).length+(e.target.value.match(/[b-df-hj-np-tv-z]/g) || []).length+(e.target.value.match(/[aeiou]/g) || []).length
+        })
+      }
+
     render(){
         return(
             <section>
-                
+                <div class="form-group">
+                    <label for="escreva">Escreva alguma coisa</label>
+                    <input type="text" class="col-md-4 form-control" id="exampleInputEmail1" onKeyUp={(e)=>this.verificaDigito(e) }/>
+                    <br/>
+                    <span>total:{this.state.caracteres}</span>
+                    <br/>
+                    <span>Vogais: {this.state.vogais}</span>
+                    <br/>
+                    <span>Especiais:{this.state.especial}</span>
+                    <br/>
+                    <span>Consoante:{this.state.consoantes}</span>
+                    <br/>
+                    <span>Numeros:{this.state.outros}</span>
+                </div>
             </section>
         )
     }
 }
+/*
+https://stackoverflow.com/questions/7657824/count-the-number-of-integers-in-a-string
+*/
